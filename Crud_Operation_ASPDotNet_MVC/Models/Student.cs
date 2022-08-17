ee57a6c1c07bb6df867e524403165fda70961d20
Zulqarnain Qasim <zulqarnain.qasim@outlook.com>
2022-08-18 01:47:15 +0500

    Student Model Crerated

---
+++ Crud_Operation_ASPDotNet_MVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud_Operation_ASPDotNet_MVC.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string year { get; set; }
+    }
+}
