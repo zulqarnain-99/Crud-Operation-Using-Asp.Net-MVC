5f0023f3c77d98a5e3b8f70b55b5e6b22107652c
Zulqarnain Qasim <zulqarnain.qasim@outlook.com>
2022-08-18 02:00:51 +0500

    MVC Application Created

---
+++ Crud_Operation_ASPDotNet_MVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Crud_Operation_ASPDotNet_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Crud_Operation_ASPDotNet_MVC.Data.StudentDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Crud_Operation_ASPDotNet_MVC.Data.StudentDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
